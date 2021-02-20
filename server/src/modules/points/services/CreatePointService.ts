74bf4a13d460b8a85e63695276c09a8969188a86
robertokbr <robertojuniordev@gmail.com>
2021-02-20 20:20:12 -0300

    Add CreatePointService

---
+++ server/src/modules/points/services/CreatePointService.ts
@@ -0,0 +1,57 @@
+import { injectable, inject } from 'tsyringe';
+import Point from '../infra/typeorm/entities/Point';
+
+import IPointsRepository from '../repositories/IPointsRepository';
+
+interface Request extends Omit<ICreatePointDTO, 'pictures'> {
+  pictures: string[];
+}
+
+@injectable()
+class CreatePointService {
+  constructor(
+    @inject('PointsRepository')
+    private pointsRepository: IPointsRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
+  ) {}
+
+  public async execute({
+    user_id,
+    adress,
+    category,
+    description,
+    latitude,
+    longitude,
+    pictures,
+    price,
+    title,
+    type,
+  }: Request): Promise<Point> {
+    const serializedPictures = pictures.join('&');
+
+    await Promise.all(
+      pictures.map(async picture => {
+        this.storageProvider.saveFile(picture);
+      }),
+    );
+
+    const point = await this.pointsRepository.createPoint({
+      pictures: serializedPictures,
+      adress,
+      category,
+      description,
+      latitude,
+      longitude,
+      price,
+      title,
+      type,
+      user_id,
+    });
+
+    return point;
+  }
+}
+
+export default CreatePointService;
