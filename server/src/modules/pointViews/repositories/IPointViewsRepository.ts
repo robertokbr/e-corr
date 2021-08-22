a1b264a459791ef1092093718f4fdec6b4bea544
robertokbr <robertojuniordev@gmail.com>
2021-08-21 21:17:53 -0300

    refactor: remove repository

--- server/src/modules/pointViews/repositories/IPointViewsRepository.ts
+++
@@ -1,8 +0,0 @@
-import PointViews from '../infra/typeorm/entities/PointView';
-
-interface IPointViewsRepository {
-  createView(point_id: string): Promise<PointViews>;
-  getNumberOfViews(point_id: string): Promise<number>;
-}
-
-export default IPointViewsRepository;
