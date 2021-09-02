3b5227508876a5f6bb4e72ddfffbe23bd1d505f1
robertokbr <robertojuniordev@gmail.com>
2021-09-02 19:28:34 -0300

    feat: update to create point with new shape

--- server/src/modules/points/services/CreatePointService.ts
+++ server/src/modules/points/services/CreatePointService.ts
@@ -1,10 +1,22 @@
+import AppError from '@shared/errors/AppError';
 import { injectable, inject } from 'tsyringe';
 import Point from '../infra/typeorm/entities/Point';
+import ICategoriesRepository from '../repositories/ICategoriesRepository';
 
 import IPointsRepository from '../repositories/IPointsRepository';
+import ITagsRepository from '../repositories/ITagsRepository';
 
-interface Request extends Omit<ICreatePointDTO, 'pointImages'> {
+interface Request {
+  category_id: number;
+  description: string;
+  longitude: string;
+  latitude: string;
+  user_id: string;
+  address: string;
   images: string[];
+  title: string;
+  price: string;
+  tags: string[];
 }
 
 @injectable()
@@ -15,35 +27,68 @@ class CreatePointService {
 
     @inject('StorageProvider')
     private storageProvider: IStorageProvider,
+
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository,
+
+    @inject('TagsRepository')
+    private tagsRepository: ITagsRepository,
   ) {}
 
   public async execute({
+    category_id,
     description,
     longitude,
-    category,
     latitude,
     address,
     user_id,
     images,
     price,
     title,
-    type,
+    tags,
   }: Request): Promise<Point> {
     const pointImages = await Promise.all(
       images.map(image => this.storageProvider.saveFile(image)),
     );
 
+    const category = await this.categoriesRepository.findCategoryById(
+      category_id,
+    );
+
+    if (!category) {
+      throw new AppError('You must provide a valid category');
+    }
+
+    const tag_names = tags.map(tag => tag.toLowerCase());
+
+    const existent_tags = await this.tagsRepository.findTagsByNames(tag_names);
+
+    const existent_tag_names = existent_tags.map(tag => tag.name);
+
+    const non_existent_tag_names = tag_names.filter(
+      tag_name => !existent_tag_names.includes(tag_name),
+    );
+
+    const new_tags = await Promise.all(
+      non_existent_tag_names.map(tag_name =>
+        this.tagsRepository.createTag(tag_name),
+      ),
+    );
+
     const point = await this.pointsRepository.createPoint({
+      category_id,
       description,
       pointImages,
       longitude,
-      category,
       latitude,
       address,
       user_id,
       price,
       title,
-      type,
+      tag_ids: [
+        ...existent_tags.map(({ id }) => id),
+        ...new_tags.map(({ id }) => id),
+      ],
     });
 
     return point;
