0aa862dc10a4d175b8782c77995e0a0be56d49b5
robertokbr <robertojuniordev@gmail.com>
2021-09-03 20:57:12 -0300

    feat(server): register new repositories

--- server/src/shared/container/index.ts
+++ server/src/shared/container/index.ts
@@ -7,6 +7,12 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IPointsRepository from '@modules/points/repositories/IPointsRepository';
 import PointsRepository from '@modules/points/infra/typeorm/repositories/PointsRepository';
+import ICategoriesRepository from '@modules/points/repositories/ICategoriesRepository';
+import CategoriesRepository from '@modules/points/infra/typeorm/repositories/CategoriesRepository';
+import IPointViewsRepository from '@modules/points/repositories/IPointViewsRepository';
+import PointViewsRepository from '@modules/points/infra/typeorm/repositories/PointViewsRepository';
+import ITagsRepository from '@modules/points/repositories/ITagsRepository';
+import TagsRepository from '@modules/points/infra/typeorm/repositories/TagsRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
@@ -17,3 +23,15 @@ container.registerSingleton<IPointsRepository>(
   'PointsRepository',
   PointsRepository,
 );
+
+container.registerSingleton<IPointViewsRepository>(
+  'PointViewsRepository',
+  PointViewsRepository,
+);
+
+container.registerSingleton<ICategoriesRepository>(
+  'CategoriesRepository',
+  CategoriesRepository,
+);
+
+container.registerSingleton<ITagsRepository>('TagsRepository', TagsRepository);
