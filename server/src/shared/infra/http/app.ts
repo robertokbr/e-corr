5679fda2d066201effa7cc739b69e3ebfc636aea
robertokbr <robertojuniordev@gmail.com>
2021-09-03 20:55:43 -0300

    add new app middlewares

--- server/src/shared/infra/http/app.ts
+++ server/src/shared/infra/http/app.ts
@@ -2,32 +2,23 @@ import 'reflect-metadata';
 import 'express-async-errors';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
 
-import AppError from '@shared/errors/AppError';
 import upload from '@config/upload';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
+import appError from './middleware/appError';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
-
-app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internal Server error',
-  });
-});
+app.use(helmet());
+app.use(compression());
+app.use(appError);
 
 app.use('/files', express.static(upload.uploadFolder));
 
---
+++ server/src/shared/infra/http/middleware/appError.ts
@@ -0,0 +1,21 @@
+import AppError from '@shared/errors/AppError';
+import { Request, Response, NextFunction } from 'express';
+
+export default function appError(
+  error: Error,
+  request: Request,
+  response: Response,
+  _: NextFunction,
+) {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server error',
+  });
+}
